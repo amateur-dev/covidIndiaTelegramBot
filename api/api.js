bdbc2a179a1434119dcb821f4158e85cc135e297
amateur-dev <me@dipeshsukhani.dev>
2020-04-06 08:21:11 +0800

    zeit starter

--- api/api.js
+++ api/api.js
@@ -7,16 +7,19 @@ let telegram_url =
   (process.env.TOKEN || KEYS.parsed.TOKEN) +
   "/sendMessage";
 
-api.all("/", async (req, res) => {
-  const message  = req.body.message;
-  if (message == "getdata") {
-    var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
-    res.send(JSON.stringify(reply.data.data))
-    res.end();
-  } else {
-    res.send("send the correct message please")
-    res.end()
-  }
+api.get("/", async (req, res) => {
+  var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
+  res.send(JSON.stringify(reply.data.data))
+  res.end();
+  // const message  = req.body.message;
+  // if (message == "getdata") {
+  //   var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
+  //   res.send(JSON.stringify(reply.data.data))
+  //   res.end();
+  // } else {
+  //   res.send("send the correct message please")
+  //   res.end()
+  // }
 });
 
 module.exports = api;
