2d22ca46081aef3d509023f89e58250964c40dc3
amateur-dev <me@dipeshsukhani.dev>
2020-04-06 08:25:04 +0800

    zeit attempt 2

--- api/api.js
+++ api/api.js
@@ -2,11 +2,6 @@ const api = require('express').Router();;
 const KEYS = require("dotenv").config();
 const axios = require("axios");
 
-let telegram_url =
-  "https://api.telegram.org/bot" +
-  (process.env.TOKEN || KEYS.parsed.TOKEN) +
-  "/sendMessage";
-
 api.get("/", async (req, res) => {
   var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
   res.send(JSON.stringify(reply.data.data))
