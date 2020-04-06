d709313a87faaf41b88c0209288edd0763bfc0a9
amateur-dev <me@dipeshsukhani.dev>
2020-04-06 08:17:30 +0800

    working serverless api code

--- api/api.js
+++ api/api.js
@@ -1,6 +1,4 @@
-var express = require("express");
-var api = express();
-var bodyParser = require("body-parser");
+const api = require('express').Router();;
 const KEYS = require("dotenv").config();
 const axios = require("axios");
 
@@ -9,14 +7,7 @@ let telegram_url =
   (process.env.TOKEN || KEYS.parsed.TOKEN) +
   "/sendMessage";
 
-api.use(bodyParser.json());
-api.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-api.all("/api", async (req, res) => {
+api.all("/", async (req, res) => {
   const message  = req.body.message;
   if (message == "getdata") {
     var reply = await axios.get("https://api.rootnet.in/covid19-in/stats/latest")
--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 const KEYS = require("dotenv").config();
 const axios = require("axios");
 
-const ourFunction = require("./api/api");
+const api = require("./api/api")
 
+var app = express();
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
-app.all("/", ourFunction);
+app.all("/", (req, res) => {
+    res.status(200, { "Content-Type": "text/html" });
+    res.end("<html>Thank you for visting the landing page</html>");
+
+})
+
+app.use("/api", api);
 
 let port = (process.env.PORT || 3000);
 app.listen(port, () => console.log(`Server is listening on port: ${port}`))
